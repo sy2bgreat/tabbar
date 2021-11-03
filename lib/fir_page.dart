f012b5096f4bfaf484058b7be605c33dfdfd3d0f
이소윤 <s_lee77176@fanshaweonline.ca>
2021-11-03 21:21:26 +0900

    class with stless widget

--- lib/fir_page.dart
+++ lib/fir_page.dart
@@ -9,20 +9,46 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ListView.builder(
-        itemBuilder: (context, position) {
-          return Card(
-              child: Row(
-            children: [
-              Image.asset(list[position].imagePath,
-                  height: 100, width: 100, fit: BoxFit.contain),
-              Text("   ${list[position].animalName}"),
-            ],
-          ));
-        },
-        itemCount: list.length,
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return GestureDetector(
+                child: Card(
+                  child: Row(
+                    children: [
+                      Image.asset(list[position].imagePath,
+                          height: 100, width: 100, fit: BoxFit.contain),
+                      Text("   ${list[position].animalName}"),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  _showDialog(
+                      context, list[position].kind, list[position].animalName);
+                });
+          },
+          itemCount: list.length,
+        ),
       ),
-    ));
+    );
+  }
+
+  _showDialog(BuildContext context, String kind, String name) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(name),
+          content: Text("이 동물은 품종이 $kind 입니다."),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("close"))
+          ],
+        );
+      },
+    );
   }
 }
